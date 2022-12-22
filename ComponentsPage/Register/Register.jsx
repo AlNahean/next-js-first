e439fb292488ef8b17e33bac19de4bd2b30e9cf8
AlNahean <nahean.fardous@gmail.com>
2022-12-22 16:27:24 +0600

    added register folder

---
+++ ComponentsPage/Register/Register.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div className=' register-page'>Register</div>
+  )
+}
+
+export default Register
---
+++ pages/register.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Register from '../ComponentsPage/Register/Register'
+
+
+const register = () => {
+  return (
+    <>
+        <Register/>
+    </>
+  )
+}
+
+export default register
